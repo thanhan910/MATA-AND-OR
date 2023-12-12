34a7935edd4f3672a29f0f0dfb05ebdaac6e5901
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2023-12-13 01:49:04 +1100

    Change sys_rewards for new tree type

--- CalcRewards.py
+++ CalcRewards.py
@@ -1,3 +1,6 @@
+from GenAndOrTree import Node, NodeType
+
+
 def task_reward(task, agents, gamma=1):
     # task is represented by a list of capabilities it requires, agents is a list agents, where each represented by a list cap contribution values
     """
@@ -36,40 +39,36 @@ def sys_rewards_tasks(tasks, agents, coalition_structure, gamma=1):
     )
 
 
-def sys_rewards_tree_agents(tree, root_node_type, tasks, agents, allocation_structure, node_values = {}, gamma=1):
+def sys_rewards_tree_agents(tree_info : list[Node], tasks, agents, allocation_structure, node_values : list = None, root_node_index=-1, gamma=1):
     """
     Calculate the reward of the system, given the allocation structure: agent -> task
     """
-    if isinstance(tree, int):
-        if tree in node_values:
-            return node_values[tree]
-        else:
-            return task_reward(tasks[tree], [agent for i, agent in enumerate(agents) if allocation_structure[i] == tree], gamma)
-    rewards = [
-        sys_rewards_tree_agents(subtree, root_node_type, tasks, agents, allocation_structure, gamma)
-        for subtree in tree
-    ]
-    if root_node_type == "AND":
-        return sum(rewards)
-    elif root_node_type == "OR":
-        return max(rewards)
+    def sys_rewards_node(node : Node):
+        if node.node_type == NodeType.LEAF or node.node_type == NodeType.DUMMY:
+            if node_values is not None and node_values != []:
+                return node_values[node.node_id]
+            return task_reward(tasks[node.node_id], [agent for i, agent in enumerate(agents) if allocation_structure[i] == node.node_id], gamma)
+        rewards = [sys_rewards_node(tree_info[i]) for i in node.children_ids]
+        if node.node_type == NodeType.AND:
+            return sum(rewards)
+        elif node.node_type == NodeType.OR:
+            return max(rewards)
+    return sys_rewards_node(tree_info[root_node_index])
 
 
-def sys_rewards_tree_tasks(tree, root_node_type, tasks, agents, coalition_structure, node_values = {}, gamma=1):
+def sys_rewards_tree_tasks(tree_info, root_node_type, tasks, agents, coalition_structure, node_values : list = None, root_node_index=-1, gamma=1):
     """
     Calculate the reward of the system, given the coalition structure: task -> agents (coalition)
     """
-    if isinstance(tree, int):
-        if tree in node_values:
-            return node_values[tree]
-        else:
-            return task_reward(tasks[tree], [agents[i] for i in coalition_structure[tree]], gamma)
-    rewards = [
-        sys_rewards_tree_tasks(subtree, root_node_type, tasks, agents, coalition_structure, gamma)
-        for subtree in tree
-    ]
-    if root_node_type == "AND":
-        return sum(rewards)
-    elif root_node_type == "OR":
-        return max(rewards)
+    def sys_rewards_node(node : Node):
+        if node.node_type == NodeType.LEAF or node.node_type == NodeType.DUMMY:
+            if node_values is not None and node_values != []:
+                return node_values[node.node_id]
+            return task_reward(tasks[node.node_id], [agents[i] for i in coalition_structure[node.node_id]], gamma)
+        rewards = [sys_rewards_node(tree_info[i]) for i in node.children_ids]
+        if node.node_type == NodeType.AND:
+            return sum(rewards)
+        elif node.node_type == NodeType.OR:
+            return max(rewards)
+    return sys_rewards_node(tree_info[root_node_index])
 
