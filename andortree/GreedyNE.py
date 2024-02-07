2d4272ac422c4a80d2bd89c41f5e4f81acd9ce55
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-02-07 23:38:05 +1100

    GreedyNE for allocation structure

--- andortree/GreedyNE.py
+++ andortree/GreedyNE.py
@@ -567,3 +567,171 @@ def adGreedyNE(
         iteration_count,
         re_assignment_count,
     )
+
+
+def alGreedyNE(
+        agents : list[list[float]], 
+        tasks : list[list[int]],
+        constraints : tuple[list[list[int]], list[list[int]]],
+        original_allocation_structure : dict[int, int] = None,
+        selected_tasks : list[int] = None,
+        selected_agents : list[int] = None,
+        eps=0, 
+        gamma=1,
+        dummy_task_id = None
+    ):
+    """
+    GreedyNE on a subset of tasks.
+
+    The target is to focus all agents on working on only the selected tasks and skip all other tasks.
+    """
+    re_assignment_count = 0
+    a_taskInds = constraints[0]
+    agent_num = len(agents)
+    task_num = len(tasks)
+
+    if dummy_task_id is None:
+        dummy_task_id = len(tasks)
+
+    task_selected = {}
+    if selected_tasks is None:
+        selected_tasks = list(range(len(tasks)))
+        task_selected = { j : True for j in range(len(tasks)) }
+    else:    
+        task_selected = { j : False for j in range(len(tasks)) }
+        for j in selected_tasks:
+            task_selected[j] = True
+    task_selected[dummy_task_id] = True
+
+
+    if selected_agents is None:
+        selected_agents = list(range(0, agent_num))
+        agent_selected = { i : True for i in range(len(agents)) }
+
+    else:
+        agent_selected = { i : True for i in range(len(agents)) }
+        for i in selected_agents:
+            agent_selected[i] = True
+
+
+    allocation_structure = { i : dummy_task_id for i in selected_agents }
+    for i, j in original_allocation_structure.items():
+        if agent_selected[i]:
+            allocation_structure[i] = j
+
+    coalition_structure = { j : [] for j in selected_tasks + [dummy_task_id] }
+
+    for i, j in allocation_structure.items():
+        coalition_structure[j].append(i)
+
+    cur_con = { i : agent_contribution(agents, tasks, i, j, coalition_structure[j], constraints, gamma) if j != dummy_task_id else 0 for i, j in allocation_structure.items() }
+        
+
+    task_cons = {
+        i : {
+            j : agent_contribution(agents, tasks, i, j, coalition_structure[j], constraints, gamma)
+            if j != dummy_task_id and task_selected[j]
+            else 0 if j == dummy_task_id
+            else float("-inf")
+            for j in a_taskInds[i] + [dummy_task_id]
+        }
+        for i in selected_agents
+    }
+    # the last 0 indicate not allocated
+
+    move_vals = {
+        i : {
+            j : task_cons[i][j] - cur_con[i] 
+            if j == dummy_task_id or task_selected[j]
+            else float("-inf")
+            for j in a_taskInds[i] + [dummy_task_id]
+        }
+        for i in selected_agents
+    }
+
+
+    max_moves = {
+        i : max(move_vals[i].items(), key=lambda x: x[1])
+        for i in selected_agents
+    }
+
+    iteration_count = 0
+    while True:
+        iteration_count += 1
+        feasible_choices = [i for i in selected_agents if max_moves[i][1] > 0]
+        if feasible_choices == []:
+            break  # reach NE solution
+        # when eps = 1, it's Random, when eps = 0, it's Greedy
+        if np.random.uniform() <= eps:
+            # exploration: random allocation
+            a_index = np.random.choice(feasible_choices)
+            t_index = max_moves[a_index][0]
+        else:
+            # exploitation: allocationelse based on reputation or efficiency
+            best_move = max(max_moves.items(), key=lambda x: x[1][1])
+            a_index = best_move[0]
+            t_index = best_move[1][0]
+
+        # perfom move
+        old_t_index = allocation_structure[a_index]
+        allocation_structure[a_index] = t_index
+        coalition_structure[t_index].append(a_index)
+
+        # update agents in the new coalition
+        affected_a_indexes = []
+        affected_t_indexes = []
+        if t_index != dummy_task_id:
+            affected_a_indexes.extend(coalition_structure[t_index])
+            affected_t_indexes.append(t_index)
+
+            # task_cons[i][t_index]
+            for i in coalition_structure[t_index]:
+                if agent_selected[i]:
+                    task_cons[i][t_index] = agent_contribution(agents, tasks, i, t_index, coalition_structure[t_index], constraints, gamma)
+                    cur_con[i] = task_cons[i][t_index]
+        else:
+            affected_a_indexes.append(a_index)
+            task_cons[a_index][t_index] = 0
+            cur_con[a_index] = 0
+
+        # update agent in the old coalition (if applicable)
+        if (old_t_index != dummy_task_id):  
+            # if agents indeed moved from another task, we have to change every agent from the old as well
+            re_assignment_count += 1
+            coalition_structure[old_t_index].remove(a_index)
+            affected_a_indexes.extend(coalition_structure[old_t_index])
+            affected_t_indexes.append(old_t_index)
+            for i in coalition_structure[old_t_index]:
+                task_cons[i][old_t_index] = agent_contribution(agents, tasks, i, old_t_index, coalition_structure[old_t_index], constraints, gamma)
+                cur_con[i] = task_cons[i][old_t_index]
+
+        for i in affected_a_indexes:
+            move_vals[i] = {
+                j : task_cons[i][j] - cur_con[i]
+                if j == dummy_task_id or task_selected[j]
+                else float("-inf")
+                for j in a_taskInds[i] + [dummy_task_id]
+            }
+
+
+        ## update other agents w.r.t the affected tasks
+        for t_ind in affected_t_indexes:
+            for i in range(0, agent_num):
+                if (i not in coalition_structure[t_ind]) and (t_ind in a_taskInds[i]):
+                    task_cons[i][t_ind] = agent_contribution(agents, tasks, i, t_ind, coalition_structure[t_ind], constraints, gamma)
+                    move_vals[i][t_ind] = task_cons[i][t_ind] - cur_con[i]
+
+        max_moves = {
+            i : max(move_vals[i].items(), key=lambda x: x[1])
+            for i in selected_agents
+        }
+
+
+
+    return (
+        coalition_structure,
+        allocation_structure,
+        sys_rewards_tasks(tasks, agents, coalition_structure, gamma),
+        iteration_count,
+        re_assignment_count,
+    )
