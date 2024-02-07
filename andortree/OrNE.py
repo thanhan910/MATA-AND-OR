ead5f9e880a6a479b4098abc47b77776c7ab6ebf
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-02-08 02:02:33 +1100

    Complete OrNE

--- andortree/OrNE.py
+++ andortree/OrNE.py
@@ -1,12 +1,24 @@
 from .node_type import NodeType
-from .GreedyNE import adGreedyNE
+from .GreedyNE import adGreedyNE, alGreedyNE
 from .upper_bound import upper_bound_subsytem
 from .rewards import task_reward, sys_rewards_tasks
+from .tree_utils import traverse_tree
+
+import numpy as np
 
 
 
-def update_leaves_info(updated_node, leaves_info, children_info, parent_info, node_type_info):
+
+def update_leaves_info(
+        updated_node : int, 
+        leaves_info : dict[int, list[int]],
+        children_info : dict[int, list[int]],
+        parent_info : dict[int, int],
+        node_type_info : dict[int, NodeType]
+    ):
     """
+    Update the leaves_info for all ancestors of the updated_node.
+
     updated_node: the node that has just been updated in leaves_info
     """
     current_node = updated_node
@@ -93,13 +105,88 @@ def ao_search(
     return total_reward, best_leafs_solution, solution_path_children_info, solution_path_leaves_info
 
 
+def upperbound_node(
+        ubcv_info : dict[int, np.ndarray],
+        capabilities : list[int], 
+        agents : list[list[float]],
+        nodes_constraints : tuple[list[list[int]], dict[int, list[int]]],
+        query_nodeId=0 
+    ):
+    """
+    Calculate the upper bound of the system reward, i.e. at the root of the AND-OR goal tree.
+    """
+    nodes_agents = nodes_constraints[1]
+
+    caps_ranked = [sorted([agents[i][c] for i in nodes_agents[query_nodeId]], reverse=True) for c in capabilities]
+
+    cap_req_num = ubcv_info[query_nodeId]
+    
+    return sum([sum(caps_ranked[c][:int(cap_req_num[c])]) for c in capabilities])
+
+
+def get_upperbound_node_descendants(
+        query_nodeId : int,
+        ubcv_info : dict[int, np.ndarray],
+        children_info : dict[int, list[int]],
+        node_type_info : dict[int, NodeType],
+        capabilities : list[int], 
+        agents : list[list[float]],
+        nodes_constraints : tuple[list[list[int]], dict[int, list[int]]],
+    ):
+    """
+    Calculate the upper bound of the reward (utility) at each descendant of the queried node in the AND-OR goal tree.
+    
+    Calculate the upper bound of the reward (utility) at each node of the AND-OR goal tree.
+
+    Refine the upper bound by taking the minimum of the upper bound calculated from the children nodes, and the upper bound calculated from the current node.
+
+    """
+
+    def _upperbound_node(query_nodeId):
+        """
+        Calculate the upper bound of the system reward, i.e. at the root of the AND-OR goal tree.
+        """
+        nodes_agents = nodes_constraints[1]
+
+        caps_ranked = [sorted([agents[i][c] for i in nodes_agents[query_nodeId]], reverse=True) for c in capabilities]
+
+        cap_req_num = ubcv_info[query_nodeId]
+        
+        return sum([sum(caps_ranked[c][:int(cap_req_num[c])]) for c in capabilities])
+
+    descendant_nodes = list(traverse_tree(children_info, root_node_id=query_nodeId))
+    
+    nodes_upper_bound = { node_id : _upperbound_node(node_id) for node_id in descendant_nodes }
+
+    nodes_upper_bound_min = { node_id : 0 for node_id in descendant_nodes }
+
+    def _min_upper_bound(node_id : int):
+        node_type = node_type_info[node_id]
+
+        if node_type == NodeType.LEAF:
+            nodes_upper_bound_min[node_id] = nodes_upper_bound[node_id]
+
+        elif node_type == NodeType.OR:
+            nodes_upper_bound_min[node_id] = max(_min_upper_bound(child_id) for child_id in children_info[node_id])
+
+        elif node_type == NodeType.AND:
+            nodes_upper_bound_min[node_id] = sum(_min_upper_bound(child_id) for child_id in children_info[node_id])
+
+        else:
+            raise Exception("Unsupported node type")
+
+        nodes_upper_bound_min[node_id] = min(nodes_upper_bound[node_id], nodes_upper_bound_min[node_id])
+        return nodes_upper_bound_min[node_id]
+        
+    _min_upper_bound(query_nodeId)
+
+    return nodes_upper_bound_min
+
 
 def OrNE(
         node_type_info: dict[int, NodeType], 
         children_info: dict[int, list[int]], 
-        parent_info: dict[int, int],
-        nodes_upper_bound: dict[int, float],
-        nodes_upper_bound_min: dict[int, float],
+        ubcv_info : dict[int, np.ndarray],
         leaf2task: dict[int, int],
         task2leaf: dict[int, int],
         leaves_info: dict[int, list[int]],
@@ -112,12 +199,15 @@ def OrNE(
         gamma=1,
         root_node_id=0,
     ):
+    
+    task_num = len(tasks)
+    agent_num = len(agents)
 
-    myGreedyNE = lambda original_coalition_structure, selected_tasks, selected_agents: adGreedyNE(
+    myGreedyNE = lambda original_allocation_structure, selected_tasks, selected_agents: alGreedyNE(
         agents=agents, 
         tasks=tasks, 
         constraints=constraints, 
-        original_coalition_structure=original_coalition_structure, 
+        original_allocation_structure=original_allocation_structure,
         selected_tasks=selected_tasks, 
         selected_agents=selected_agents, 
         eps=eps, 
@@ -131,85 +221,118 @@ def OrNE(
         root_node_id=root_node_id,
     )
 
+    my_get_upper_bound = lambda node_id, agents: get_upperbound_node_descendants(
+        query_nodeId=node_id,
+        ubcv_info=ubcv_info,
+        children_info=children_info,
+        node_type_info=node_type_info,
+        capabilities=capabilities,
+        agents=agents,
+        nodes_constraints=constraints,
+    )
+
     if coalition_structure is None or coalition_structure == {}:
         coalition_structure = {j: [] for j in range(0, len(tasks))}
         coalition_structure[len(tasks)] = list(range(0, len(agents)))  # default coalition structure, the last one is dummy coalition
 
+    allocation_structure_global = {i: task_num for i in range(0, agent_num)}
+    for j in coalition_structure:
+        for i in coalition_structure[j]:
+            allocation_structure_global[i] = j
 
-    def aos_helper(node_id, agents_group, coalition_structure_0 = None):
+
+    def aos_helper(node_id, agents_group, allocation_structure_0 = None):
+
+        node_type = node_type_info[node_id]
+
+        if node_type == NodeType.LEAF:
+            task_id = leaf2task[node_id]
+            new_allocation_structure = { i: task_id for i in agents_group }
+            new_coalition_structure = { task_id: agents_group }
+            reward_value = task_reward(tasks[task_id], [agents[i] for i in agents_group], gamma)
+            return new_allocation_structure, reward_value
+
+
 
         descendant_leaves = leaves_info[node_id]
         descendant_tasks = [task2leaf[task_id] for task_id in descendant_leaves]
 
-        # Initialize coalition structure
-        if coalition_structure_0 is None:
+        # Initialize allocation structure
+        if allocation_structure_0 is None:
             # Perform GreedyNE on the entire system, not considering the tree structure
-            coalition_structure_1, _, _, _ = myGreedyNE(
-                original_allocation_structure=coalition_structure_0,
+            coalition_structure_1, allocation_structure_1, _, _, _ = myGreedyNE(
+                original_allocation_structure=allocation_structure_1,
                 selected_tasks=descendant_tasks,
                 selected_agents=agents_group,
             )
         else:
-            coalition_structure_1 = coalition_structure_0
+            allocation_structure_1 = allocation_structure_0
+            coalition_structure_1 = { j: [] for j in descendant_tasks + [len(tasks)] }
+            for i in allocation_structure_1:
+                for j in allocation_structure_1[i]:
+                    coalition_structure_1[j].append(i)
 
         reward_function = {
             task2leaf[task_id]: task_reward(tasks[task_id], [agents[i] for i in coalition_structure_1[task_id]], gamma)
             for task_id in descendant_tasks
         }
 
-        true_sys_reward, best_leafs_solution, st_children_info = my_ao_search(
+        true_sys_reward_1, best_leafs_solution, st_children_info = my_ao_search(
             reward_function=reward_function,
             root_node_id=node_id,
         )
 
         best_tasks_solution = [task2leaf[task_id] for task_id in best_leafs_solution]
 
-        coalition_structure_2, system_reward_2, _, _ = myGreedyNE(
-            original_coalition_structure=coalition_structure_1,
+        coalition_structure_2, allocation_structure_2, system_reward_2, _, _ = myGreedyNE(
+            original_allocation_structure=allocation_structure_1,
             selected_tasks=best_tasks_solution,
             selected_agents=agents_group,
         )
 
-        # Update coalition_structure based on coalition_structure_2
-        for j in coalition_structure_2:
-            coalition_structure[j] = coalition_structure_2[j]
+        # Update allocation_solution
+        allocation_solution = {}
+        for i in allocation_structure_2:
+            for j in allocation_structure_2[i]:
+                allocation_solution[i] = j
 
-        node_type = node_type_info[node_id]
-
-        if node_type == NodeType.LEAF:
-            return coalition_structure, system_reward_2
-        
-
-        elif node_type == NodeType.AND:
+        if node_type == NodeType.AND:
             total_reward = 0
             for child_id in children_info[node_id]:
                 child_tasks_descendants = [leaf2task[leaf_id] for leaf_id in leaves_info[child_id]]
                 child_agents_group = sum([coalition_structure_2[task_id] for task_id in child_tasks_descendants], [])
-                child_coalition_solution, child_system_reward = aos_helper(child_id, child_agents_group, coalition_structure)
-                # Update coalition_structure based on child_coalition_solution
-                for j in child_coalition_solution:
-                    coalition_structure[j] = child_coalition_solution[j]
+                child_allocation_solution, child_system_reward = aos_helper(child_id, child_agents_group, allocation_structure_2)
+                # Update allocation_solution based on child_allocation_solution
+                for j in child_allocation_solution:
+                    allocation_solution[j] = child_allocation_solution[j]
                 
                 total_reward += child_system_reward
-            return coalition_structure, total_reward
+
+            return allocation_solution, total_reward
         
         else: # if node_type == NodeType.OR:
-            final_coalition_solution = {}
             current_child_id = st_children_info[node_id][0]
             total_reward = system_reward_2
+            final_allocation_solution = allocation_structure_2.copy()
             for child_id in children_info[node_id]:
                 if child_id == current_child_id:
                     continue
-                # TODO: Upper bound
-                child_coalition_solution, child_system_reward = aos_helper(child_id, agents_group, None)
+                # Bound pruning
+                nodes_upper_bound_min = my_get_upper_bound(child_id, [agents[i] for i in agents_group])
+                reward_upper_bound = nodes_upper_bound_min[child_id]
+                if reward_upper_bound <= total_reward:
+                    continue
+                # Branch to child_id
+                child_allocation_solution, child_system_reward = aos_helper(child_id, agents_group, None)
                 if child_system_reward > total_reward:
                     total_reward = child_system_reward
-                    final_coalition_solution = child_coalition_solution
+                    final_allocation_solution = child_allocation_solution
 
             # Update allocation_solution based on final_allocation_solution
-            # Update coalition_structure based on final_coalition_solution
-            for j in final_coalition_solution:
-                coalition_structure[j] = final_coalition_solution[j]
+            for j in final_allocation_solution:
+                allocation_solution[j] = final_allocation_solution[j]
 
-            return coalition_structure, total_reward
+            return allocation_solution, total_reward
 
+
+    return aos_helper(root_node_id, list(range(0, len(agents))), allocation_structure_global)
